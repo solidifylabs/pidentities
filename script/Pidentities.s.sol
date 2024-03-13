549726d453588e14505b52d2a08508e8a6175b2c
Arran Schlosberg <me@arranschlosberg.com>
2024-03-13 21:46:22 +0000

    feat: LaTeX for generating token images

--- script/Pidentities.s.sol
+++ script/Pidentities.s.sol
@@ -20,7 +20,7 @@ contract PidentitiesDeploy is Script {
         airdrops[0] = Pidentities.Mint({
             to: 0x34202f199ef058302DcceD326a0105fe2Db53E12,
             c: Pidentities.Contract({
-                name: unicode"Bailey–Borwein–Plouffe%20formula",
+                name: unicode"The%20Bailey–Borwein–Plouffe%20formula",
                 salt: 0,
                 deployedCode: hex"5f5f5f600160046005600660088460fc1b600281028582025f5b858b018404878c01850401888c01840401898c018304038a1c01848b019a508a891b9b508a891c996103f1116019575f5260fc60205260405ff3"
             })
@@ -29,7 +29,7 @@ contract PidentitiesDeploy is Script {
         airdrops[1] = Pidentities.Mint({
             to: ARRAN, // TODO
             c: Pidentities.Contract({
-                name: "Basel%20problem",
+                name: "The%20Basel%20problem",
                 salt: 0,
                 deployedCode: hex"60016006607e1b815f5b81820283040183820191620710401160095792505050607e1b600160808182821b038383828211828186021c925084861c94508586861b039350818186021b915050828211828186021c925084861c94508586861b039350818186021b915050828211828186021c925084861c94508586861b039350818186021b915050828211828186021c925084861c94508586861b039350818186021b915050828211828186021c925084861c94508586861b039350818186021b915050828211828186021c925084861c94508586861b039350818186021b915050828211828186021c925084861c94508586861b039350818186021b9150508086048101851c90508086048101851c90508086048101851c90508086048101851c90508086048101851c90508086048101851c90508086048101851c90505f52607e60205260405ff3"
             })
@@ -38,7 +38,7 @@ contract PidentitiesDeploy is Script {
         airdrops[2] = Pidentities.Mint({
             to: 0x46622E91F95F274f4f76460B38d1F5E00905f767,
             c: Pidentities.Contract({
-                name: unicode"Madhava–Leibniz%20formula",
+                name: unicode"The%20Madhava–Leibniz%20series",
                 salt: 0,
                 deployedCode: hex"620833c0600180811b8160781b805b84841b8401820401838503841b840182049003828503948310600e57821b5f52607860205260405ff3"
             })
@@ -47,7 +47,7 @@ contract PidentitiesDeploy is Script {
         airdrops[3] = Pidentities.Mint({
             to: 0x16cCd2a1346978e27FDCbda43569E251C4227341,
             c: Pidentities.Contract({
-                name: "Limiting%20sequence",
+                name: "A%20limiting%20sequence",
                 salt: 0,
                 deployedCode: hex"5f58607a81811b805b8385851b018204820102821c6103e885850195106008578002821c8490045f52607a60205260405ff3"
             })
@@ -74,7 +74,7 @@ contract PidentitiesDeploy is Script {
         airdrops[6] = Pidentities.Mint({
             to: 0xA85572Cd96f1643458f17340b6f0D6549Af482F5,
             c: Pidentities.Contract({
-                name: "Wallis%20product",
+                name: "The%20Wallis%20product",
                 salt: 0,
                 deployedCode: hex"607f620498806001821b5b81820260021b6001810390841b0402821c6001820391600110600a5760011b5f52607f60205260405ff3"
             })
